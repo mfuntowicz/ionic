46f0b1892bccf6c5b3078058b6f6f4e16892ff82
Morgan Funtowicz <funtowiczmo@gmail.com>
2026-03-30 23:21:54 +0200

    misc(memory): reuse ionic_align_[up/down]_sz

--- lib/platform/linux/memory.c
+++ lib/platform/linux/memory.c
@@ -5,6 +5,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include "ionic/types.h"
+#include "ionic/utils.h"
 
 #define IONIC_HOST_MAP_MAGIC 0x31434948u /* "HIC1" little-endian tag */
 
@@ -24,21 +25,9 @@ static const size_t ionic_host_hdr_user_off =
 #define MAP_HUGE_2MB (21 << MAP_HUGE_SHIFT)
 #endif
 
-static size_t ionic_round_up(size_t n, size_t align) {
-    if (align == 0u)
-        return n;
-    size_t r = n % align;
-    if (r == 0u)
-        return n;
-    size_t add = align - r;
-    if (n > SIZE_MAX - add)
-        return 0u;
-    return n + add;
-}
-
-static void *ionic_linux_host_mmap_allocate_impl(size_t user_size, enum ionic_allocation_kind kind) {
+static void *ionic_linux_host_mmap_allocate_impl(size_t size, enum ionic_allocation_kind kind) {
     (void)kind;
-    if (user_size == 0u)
+    if (size == 0u)
         return NULL;
 
     const size_t huge_2mb = 2u * 1024u * 1024u;
@@ -46,38 +35,36 @@ static void *ionic_linux_host_mmap_allocate_impl(size_t user_size, enum ionic_al
     if (page == 0u)
         return NULL;
 
-    size_t need = ionic_host_hdr_user_off + user_size;
-    if (need < user_size)
+    size_t need = ionic_host_hdr_user_off + size;
+    if (need < size)
         return NULL;
 
-    size_t map_sz = ionic_round_up(need, huge_2mb);
+    size_t map_sz = ionic_align_up_sz(need, huge_2mb);
     if (map_sz == 0u)
         map_sz = need;
 
     void *p = MAP_FAILED;
 
 #if defined(MAP_HUGETLB) && defined(MAP_HUGE_2MB)
-    p = mmap(NULL, map_sz, PROT_READ | PROT_WRITE,
-             MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_HUGE_2MB, -1, 0);
+    p = mmap(NULL, map_sz, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_HUGE_2MB, -1, 0);
 #endif
 
     if (p == MAP_FAILED) {
-        map_sz = ionic_round_up(need, page);
-        if (map_sz == 0u)
+        if ((map_sz = ionic_align_up_sz(need, page)) == 0u)
             return NULL;
-        p = mmap(NULL, map_sz, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        if (p == MAP_FAILED)
+
+        if ((p = mmap(NULL, map_sz, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED)
             return NULL;
 #ifdef MADV_HUGEPAGE
         (void)madvise(p, map_sz, MADV_HUGEPAGE);
 #endif
     }
 
-    struct ionic_host_map_hdr *hdr = (struct ionic_host_map_hdr *)p;
+    struct ionic_host_map_hdr *hdr = p;
     hdr->magic = IONIC_HOST_MAP_MAGIC;
     hdr->reserved = 0u;
     hdr->map_bytes = map_sz;
-    if (ionic_host_hdr_user_off > sizeof(struct ionic_host_map_hdr))
+    if (ionic_host_hdr_user_off > sizeof(struct ionic_host_map_hdr))  // todo(mfuntowicz) 32-bits system, do we really need it?
         memset((unsigned char *)p + sizeof(struct ionic_host_map_hdr), 0,
                ionic_host_hdr_user_off - sizeof(struct ionic_host_map_hdr));
 
