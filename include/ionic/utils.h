e7246acee72daa91f56f33c32835cfcfb55b62ac
Morgan Funtowicz <funtowiczmo@gmail.com>
2026-04-01 10:17:25 +0200

    misc: document utils method as read-only with const qualifiers

--- include/ionic/utils.h
+++ include/ionic/utils.h
@@ -24,7 +24,7 @@ static inline size_t ionic_tensor_nbytes(const struct ionic_tensor *tensor) {
 }
 
 static inline unsigned char ionic_is_power_of_two(const unsigned int n) { return n && !(n & (n - 1)); }
-static inline size_t ionic_align_down_sz(size_t v, size_t a) { return v & ~(a - 1); }
-static inline size_t ionic_align_up_sz(size_t v, size_t a) { return (v + a - 1) & ~(a - 1); }
+static inline size_t ionic_align_down_sz(const size_t v, const size_t a) { return v & ~(a - 1); }
+static inline size_t ionic_align_up_sz(const size_t v, const size_t a) { return (v + a - 1) & ~(a - 1); }
 
 #endif // IONIC_UTILS_H
