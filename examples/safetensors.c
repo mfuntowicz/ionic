4c8d2bfaaa1342d2079cb7b7714997cc25481d36
Morgan Funtowicz <funtowiczmo@gmail.com>
2026-05-22 11:38:04 +0200

    fix(examples): missing parameter in ionic_context_init

--- examples/safetensors.c
+++ examples/safetensors.c
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
         }
     }
 
-    ionic_context_init(&ctx, device, NULL);
+    ionic_context_init(&ctx, device, NULL, 0, 1);
     ionic_safetensors_init(&registry);
     ionic_safetensors_discover_tensors(&ctx, &registry, argv[1]);
 
